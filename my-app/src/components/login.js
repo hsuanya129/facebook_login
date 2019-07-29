78e704db8df098d43fd40d90a9f2dc4b0aff85c7
hsuanya129 <37055177+hsuanya129@users.noreply.github.com>
2019-07-29 16:43:23 +0800

    turn global compo into arrow func

--- my-app/src/components/login.js
+++ my-app/src/components/login.js
@@ -2,33 +2,39 @@ import React from 'react';
 import '../App.css';
 import Posts from './posts';
 
+
 class Login extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             posts_data: [],
-            hint:'',
+            user_name: '',
         };
-        //To activate its methods from outside(window)
-        window.Login = this;
     }
 
+    //Detect if connected or not, and save data of posts into this.state
     fetchPosts = (response) => {
         if (response.status === 'connected') {
-            window.FB.api('/me', function (response) {
-                window.Login.setState({
-                    hint:response.name
+
+            window.FB.api(`/me`, (response) => {
+                this.setState({
+                    user_name: response.name
                 });
             });
-            window.FB.api('/me','GET',{"fields":"feed{picture,created_time,message,from}"}, function (response) {
-                window.Login.setState({
-                    posts_data: response.feed.data
+
+            window.FB.api(`/me`, 'GET', { "fields": "feed{picture,created_time,message,from}" }, (response) => {
+                console.log(response);
+                let posts;
+                (response.feed) ? posts = response.feed.data : posts = "no posts";
+                this.setState({
+                    posts_data: posts
                 });
             });
-        } else {
-            window.Login.setState({
+            
+        } else if(response.status === 'unknown') {
+            this.setState({
                 posts_data: [],
-                hint:''
+                user_name: ''
             });
         }
     }
@@ -36,7 +42,7 @@ class Login extends React.Component {
     componentDidMount() {
 
         //Initialize
-        window.fbAsyncInit = function () {
+        window.fbAsyncInit = () => { 
             window.FB.init({
                 appId: '2858794564190467',
                 cookie: true,
@@ -44,19 +50,17 @@ class Login extends React.Component {
                 xfbml: true,
                 version: 'v3.3'
             });
-            this.console.log(this)
 
             //Called at the fb initialization, to check if user has login or not
-            window.FB.getLoginStatus(function (response) {
-                window.Login.fetchPosts(response);
+            window.FB.getLoginStatus((response) => { //Using arrowfunction to bind 'this' with component
+                this.fetchPosts(response);
             });
 
             //EventListener to check user login or logout,
             //Because login button can't call 'onlogin' in react
-            window.FB.Event.subscribe('auth.statusChange', function (response) {
-                window.Login.fetchPosts(response);
+            window.FB.Event.subscribe('auth.statusChange', (response) => {
+                this.fetchPosts(response);
             })
-            this.console.log(this.FB)
         };
 
         //Part of initializaion of fb
@@ -64,7 +68,7 @@ class Login extends React.Component {
             var js, fjs = d.getElementsByTagName(s)[0];
             if (d.getElementById(id)) { return; }
             js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v3.3&appId=2858794564190467&autoLogAppEvents=1";
+            js.src = "https://connect.facebook.net/zh_TW/sdk.js";
             fjs.parentNode.insertBefore(js, fjs);
         }(document, 'script', 'facebook-jssdk'));
 
@@ -83,7 +87,7 @@ class Login extends React.Component {
                     data-auto-logout-link="true"
                     data-use-continue-as="true">
                 </div>
-                <Posts data={this.state.posts_data} hint={this.state.hint} />
+                <Posts data={this.state.posts_data} user_name={this.state.user_name} />
             </div>
         )
     }
--- my-app/src/components/post.js
+++ my-app/src/components/post.js
@@ -9,15 +9,18 @@ class Post extends React.Component{
   render(){
     const date_temp = Date.parse(this.props.data.created_time);
     const date = new Date(date_temp);
-    
+    const formatContent =(this.props.data.message) ? this.props.data.message.replace(/↵/, '<br/>') :"";
+    console.log(formatContent)
     
     return(
       <div>
-        <p className="from">{this.props.data.from.name}</p>
-        <div className="content">
-          {this.props.data.message}
+        <p className="from">{(this.props.data.from) ? this.props.data.from.name : "By friend"}</p>
+        <pre className="content">
+          {formatContent}
+          <br/>
           {(this.props.data.picture) ? <img src={this.props.data.picture}></img> : ""}
-        </div>
+        </pre>
+
         <p className="date">{date.toString()}</p>
       </div>
     );
